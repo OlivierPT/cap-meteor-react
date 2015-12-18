c0a54e7c3017d5cd39ef1be23f3b01f3b8df7706
Olivier PILLAUD TIRARD <olivier.pillaudtirard@gmail.com>
2015-12-19 00:54:52 +0100

    App init

---
+++ server/loginService.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.upsert(
+  { service: "github" },
+  {
+    $set: {
+      clientId: "6dfcb45b1f9bec477d69",
+      secret: "9548516e581a2e462bed63a8c40a4026642ebf6d"
+    }
+  }
+);
